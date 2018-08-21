37b3dfb13811c903a2568aa48481bb6821f1153a
lowi <stephan@riddleandcode.com>
2018-08-21 11:36:04 +0200

    better test

--- golang/secureelement/random_test.go
+++ golang/secureelement/random_test.go
@@ -3,6 +3,7 @@ package secureelement_test
 import (
 	"bytes"
 	"encoding/hex"
+	"fmt"
 	"testing"
 
 	element "github.com/riddleandcode/secure-element-sdk/golang/secureelement"
@@ -10,18 +11,21 @@ import (
 
 func TestRandom(t *testing.T) {
 	zeroBytes := make([]byte, 32)
-	randomBytes := element.Random()
 
-	// fmt.Printf("zeroBytes: %d\n%s\n", len(zeroBytes), hex.Dump(zeroBytes))
-	// fmt.Printf("randomBytes:\n%s\n", hex.Dump(randomBytes))
+	const numLoops = 10
 
-	if len(randomBytes) != 32 {
-		t.Errorf("The length is incorrect, got: %d, want: %d.", len(randomBytes), 32)
+	fmt.Println("hello")
+	for i := 0; i < 10; i++ {
+		randomBytes := element.Random()
+
+		fmt.Printf("randomBytes:\n%s\n", hex.Dump(randomBytes))
+
+		if len(randomBytes) != 32 {
+			t.Errorf("The length is incorrect, got: %d, want: %d.", len(randomBytes), 32)
+		}
+
+		if bytes.Equal(zeroBytes, randomBytes) {
+			t.Errorf("The random bytes are all zero. Not very random, is it?\n%s", hex.Dump(randomBytes))
+		}
 	}
-
-	if bytes.Equal(zeroBytes, randomBytes) {
-		t.Errorf("The random bytes are all zero. Not very random, is it?\n%s", hex.Dump(randomBytes))
-	}
-
-	// test stuff here...
 }
