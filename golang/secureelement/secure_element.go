427671ca5a3d8f1e96a3cb6d000e8a7980e00a5e
lowi <stephan@riddleandcode.com>
2018-08-21 11:15:25 +0200

    switching on the secure element

--- golang/secureelement/secure_element.go
+++ golang/secureelement/secure_element.go
@@ -6,21 +6,17 @@ package secureelement
 
 //#cgo CFLAGS: -I../inc
 //#cgo LDFLAGS: -L../lib -Wl,-rpath -Wl,./ -lcryptoauth
-//#include "golang/wrapper.h"
-/*
+//#include "./wrapper.h"
 import "C"
 import (
 	"unsafe"
 )
-*/
 
 /*
 Generate 32 bytes of true, hardware-generated, as-good-as-it-gets,
 finest-entropy randomness.
 */
 func Random() []byte {
-	// randomBytes := make([]byte, C.RANDOM_NUM_SIZE)
-	// return C.getRandomNumber((*C.uint8_t)(unsafe.Pointer(&randomBytes[0])))
-	randomBytes := make([]byte, 32)
-	return randomBytes
+	randomBytes := make([]byte, C.RANDOM_NUM_SIZE)
+	return C.getRandomNumber((*C.uint8_t)(unsafe.Pointer(&randomBytes[0])))
 }
