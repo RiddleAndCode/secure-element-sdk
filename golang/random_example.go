77f682874cd344033f337ca98c1f45e87c64af95
Jürgen <juergen@riddleandcode.com>
2018-08-22 10:31:46 +0200

    changed linux arm binary - added random_example.go API examples

--- golang/random_example.go
+++ golang/random_example.go
@@ -10,9 +10,37 @@ import (
 	element "github.com/riddleandcode/go-cryptoauthlib"
 )
 
-func main(t *testing.T) {
+func main() {
 	// element.Random() returns 32 bytes of random data
 	randomBytes := element.Random()
 	// print the data to the console
 	fmt.Printf("randomBytes:\n%s\n", hex.Dump(randomBytes))
+
+
+
+	var res int
+	var random_number = element.Random()
+	fmt.Printf("Here's your random: %d\n", res)
+	fmt.Printf("%s\n", hex.Dump(random_number))
+
+
+	var digest = element.Random()
+	fmt.Printf("Here's your digest: %d\n", res)
+	fmt.Printf("%s\n", hex.Dump(digest))
+
+	var public_key []byte
+	res, public_key = element.GetPublicKey()
+	fmt.Printf("Here's your public key: %d\n", res)
+	fmt.Printf("%s\n", hex.Dump(public_key))
+
+	var signature []byte
+	res, signature = element.SignDigest(digest)
+	fmt.Printf("Here's your signature: %d\n", res)
+	fmt.Printf("%s\n", hex.Dump(signature))
+
+	var verified bool
+	res, verified = element.VerifySignedDigest( digest, signature, public_key )
+	fmt.Printf("Here's your verification: %d\n", res)
+	fmt.Printf("%t \n",verified )
+
 }
